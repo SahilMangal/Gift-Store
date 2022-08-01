5274d452a69383c49e4ead5123fcdd302d5e86b4
Sahil Mangal <sahil.mangal2220@gmail.com>
2022-07-31 20:42:05 -0400

    Add JavaScript and Stylesheet Files

---
+++ Javascript/signupScript.js
@@ -0,0 +1,7 @@
+function imageHighlight() {
+    document.getElementById("signup1Image").style.boxShadow = "1px 0px 15px white";
+}
+
+function revertHighlight() {
+    document.getElementById("signup1Image").style.boxShadow = "0px 0px 5px white";
+}
